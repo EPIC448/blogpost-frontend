28d7246e599842c7911373b7b0f88c4d99ff786f
EPIC448 <samrey2018@gmail.com>
2021-04-20 20:59:31 -0400

    created App.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
 import {connect} from 'react-redux'
 import {fetchBlogposts} from './actions/fetchBlogposts'
 import BlogpostsContainer from './containers/BlogpostsContainer'
+import './App.css';
 
 
 
