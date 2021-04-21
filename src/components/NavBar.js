8a315b66607b6253a015257192fddfecbff56523
EPIC448 <samrey2018@gmail.com>
2021-04-20 21:26:03 -0400

    NavBar.js refactored and updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,11 @@ const NavBar = ( ) => {
     
     return (
         <div>
-          <Nav className="mr-auto" navbar>
+          <Nav className="navbar1" navbar>
             <NavLink href="/blog_posts" style={{ paddingRight: '10px' }}> All Blogposts </NavLink>
         </Nav>
           
-        <Nav className="mr-auto" navbar>
+        <Nav className="navbar2" navbar>
             <NavLink href="/blog_posts/new" style={{ paddingRight: '10px' }}> Add New BlogPost </NavLink>
         </Nav>
 
