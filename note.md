e9d79458e80640cafe43fd04c06e21761e4da329
EPIC448 <samrey2018@gmail.com>
2021-04-22 17:07:31 -0400

    logo added successfully and working

--- note.md
+++ note.md
@@ -15,3 +15,5 @@ https://reactstrap.github.io/components/tooltips/
 
 ---- update App.js with index.html content-- Resources below.-  
 https://www.freecodecamp.org/news/quick-guide-to-understanding-and-creating-reactjs-apps-8457ee8f7123/
+
+ Logo address for the Top of othe page.. left--- https://images-platform.99static.com//JtU1TCib6mSwjdlOmc68d31hy_Q=/171x864:988x1681/fit-in/590x590/projects-files/60/6081/608172/03cf1f79-fd75-489d-8bc3-fb00cfb16866.jpg
--- src/App.js
+++ src/App.js
@@ -21,11 +21,19 @@ class App extends React.Component {
 
       <div className="App">
         
+       
       <video autoplay muted loop id="myVideo">
          <source src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4" type="video/mp4"/>
       </video>
-      
+
+
       <div class="content_container">
+
+        <div className="logo-main">
+            <img src="https://images-platform.99static.com//JtU1TCib6mSwjdlOmc68d31hy_Q=/171x864:988x1681/fit-in/590x590/projects-files/60/6081/608172/03cf1f79-fd75-489d-8bc3-fb00cfb16866.jpg"/>
+        </div>
+
+
             <h1 class="display-3">Snowed_IN</h1>
             <p class="lead mb-0"> All things Cold Weather</p>
         <BlogpostsContainer />
